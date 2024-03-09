3c3466a56ed8851f9075440201a73c6b2c98c629
tyvdvb <ira1980nazar@gmail.com>
2024-03-09 17:34:07 -0500

    lab04.1-3

---
+++ MyRedditApp/DataManager.swift
@@ -0,0 +1,20 @@
+//
+//  DataManager.swift
+//  MyRedditApp
+//
+//  Created by Ira Nazar on 2024-03-05.
+//
+
+import UIKit
+
+class DataManager: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
